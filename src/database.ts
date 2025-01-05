17c2835da74aea66dad086525fc6cdee14cb001d
Josianne <anne.jmfreitas@gmail.com>
2025-01-04 20:43:08 -0400

    :card_file_box: add database

---
+++ src/database.ts
@@ -0,0 +1,102 @@
+export const photos = [
+  {
+    id: '001',
+    name: 'Uma vista de um pátio de um edifício alto',
+    imgUrl: 'https://images.unsplash.com/photo-1735276680696-f1c5bca7cdef?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '002',
+    name: 'Uma rua da cidade cheia de muito tráfego à noite',
+    imgUrl: 'https://images.unsplash.com/photo-1735276680702-c008c8035c18?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '003',
+    name: 'Um prédio muito alto com muitas janelas',
+    imgUrl: 'https://images.unsplash.com/photo-1734683087904-b9d1ebd9d42a?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '004',
+    name: 'Uma quadra de basquete em frente a um prédio alto',
+    imgUrl: 'https://plus.unsplash.com/premium_photo-1692303282206-f82e1450046c?q=80&w=1875&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '005',
+    name: 'Um edifício muito alto com muitas janelas',
+    imgUrl: 'https://plus.unsplash.com/premium_photo-1692303282323-50db2a8e0b67?q=80&w=1556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '006',
+    name: 'Uma rua da cidade cheia de muito tráfego à noite',
+    imgUrl: 'https://images.unsplash.com/photo-1650302469504-99734c9df41e?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '007',
+    name: 'Um ônibus amarelo dirigindo por uma rua ao lado de edifícios altos',
+    imgUrl: 'https://images.unsplash.com/photo-1696176593187-efbb650ecf9c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '008',
+    name: 'O céu noturno sobre uma cordilheira nevada',
+    imgUrl: 'https://images.unsplash.com/photo-1735598417949-d22e9f8092ea?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '009',
+    name: 'O conteúdo de uma bolsa branca',
+    imgUrl: 'https://images.unsplash.com/photo-1735779411146-a2a6ce587cbb?q=80&w=1960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '010',
+    name: 'Uma mulher vestindo uma jaqueta de couro preta com ombros cravejados',
+    imgUrl: 'https://plus.unsplash.com/premium_photo-1731950912462-9caa3905627d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '011',
+    name: 'Um grande pássaro branco com um bico longo',
+    imgUrl: 'https://images.unsplash.com/photo-1735831435060-845f03ad57c2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '012',
+    name: 'Um edifício de tijolos vermelhos com uma porta verde',
+    imgUrl: 'https://images.unsplash.com/photo-1735835593807-575407b39ed7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '013',
+    name: 'Um guarda-chuva amarelo sentado em cima de uma praia arenosa',
+    imgUrl: 'https://images.unsplash.com/photo-1730121108538-29ea5745b61b?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '014',
+    name: 'Uma pessoa em pé em uma colina coberta de neve',
+    imgUrl: 'https://images.unsplash.com/photo-1734009589019-7c557c4e7e11?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '015',
+    name: 'Um homem andando por uma rua ao lado de prédios altos',
+    imgUrl: 'https://images.unsplash.com/photo-1733964341082-d3d7dda341ac?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '016',
+    name: 'Duas pessoas sentadas em um banco em frente a um horizonte da cidade',
+    imgUrl: 'https://images.unsplash.com/photo-1731466224983-01f32f883ea7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '017',
+    name: 'Vista aérea de uma praia e do oceano',
+    imgUrl: 'https://images.unsplash.com/photo-1734279135171-b283435831db?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '018',
+    name: 'Uma vista aérea do oceano com rochas na água',
+    imgUrl: 'https://images.unsplash.com/photo-1734279135186-4f9a1fc68ce1?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '019',
+    name: 'Um grupo de pessoas sentadas ao redor de uma mesa com pizza',
+    imgUrl: 'https://plus.unsplash.com/premium_photo-1732115973206-b6acbb6d5afc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '020',
+    name: 'Vista aérea de uma praia de areia preta',
+    imgUrl: 'https://images.unsplash.com/photo-1724795685798-6c5d1ce57940?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  }
+]
