9cc89bf465cbc9a8b6d22ebd926e65da424ed49f
Josianne <anne.jmfreitas@gmail.com>
2025-01-04 23:14:01 -0400

    🧱 update SearchBar component

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -2,23 +2,33 @@ import MagniFyingGlass from '../../assets/magnifying-glass.svg'
 
 type SearchBarProps = {
   setSearchInput: (input: string) => void
+  searchInput: string
 }
 
-export default function SearchBar({ setSearchInput }: SearchBarProps) {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+export default function SearchBar({
+  setSearchInput,
+  searchInput
+}: SearchBarProps) {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault()
     setSearchInput(event.target.value)
   }
 
   return (
-    <div className='bg-zinc-50 flex w-fit p-2 rounded-full cursor-pointer'>
+    <div className="bg-zinc-50 flex w-fit p-2 rounded-full cursor-pointer border border-transparent focus-within:border-zinc-800">
       <input
         type="search"
+        value={searchInput}
         placeholder="Buscar..."
-        onChange={handleChange}
-        className='bg-transparent'
+        onChange={handleInputChange}
+        className="bg-transparent outline-none pl-2"
       />
-      <img src={MagniFyingGlass} alt="Buscar" className='size-10 p-2 rounded-full hover:bg-zinc-400' />
+      <img
+        src={MagniFyingGlass}
+        alt="Buscar"
+        onClick={() => setSearchInput(searchInput)}
+        className="size-10 p-2 rounded-full hover:bg-zinc-300"
+      />
     </div>
   )
 }
