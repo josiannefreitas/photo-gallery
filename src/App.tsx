45de7304ab40dc57967787c785e33bb71d1d863b
Josianne <anne.jmfreitas@gmail.com>
2025-01-04 23:38:06 -0400

    :construction: work in progress (rendering components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import Footer from './components/Footer'
+import Gallery from './components/Gallery'
+import Header from './components/Header'
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <>
+      <Header />
+
+      <Gallery />
+
+      <Footer />
+    </>
+  )
 }
