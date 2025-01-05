c7bb52675f1b557965a1a15c11031f0098cc0a21
Josianne <anne.jmfreitas@gmail.com>
2025-01-04 23:24:21 -0400

    🧱 create Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className='flex justify-center items-center bg-zinc-950 h-20'>
+      <p className='text-zinc-100 text-base antialiased'>© 2025 Galeria de Fotos. Todos os direitos reservados.</p>
+    </div>
+  )
+}
