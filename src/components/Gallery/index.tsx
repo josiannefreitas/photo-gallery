f6eb74aa05fe2ab044c357d82085fe0f7d388d68
Josianne <anne.jmfreitas@gmail.com>
2025-01-04 23:15:52 -0400

    :sparkles: introduce new feature (search)

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -12,18 +12,22 @@ export default function Gallery() {
 
   return (
     <div className='mt-4 flex flex-col items-center'>
-      <SearchBar setSearchInput={setSearchInput} />
+      <SearchBar setSearchInput={setSearchInput} searchInput={searchInput} />
 
-      <div className="grid grid-cols-4 m-4">
-        {filteredPhotos.map(photo => (
-          <Card
-            key={photo.id}
-            id={photo.id}
-            name={photo.name}
-            imgUrl={photo.imgUrl}
-          />
-        ))}
-      </div>
+      {
+        filteredPhotos.length > 0 ?
+        <div className="grid grid-cols-4 m-4">
+          {filteredPhotos.map(photo => (
+            <Card
+              key={photo.id}
+              id={photo.id}
+              name={photo.name}
+              imgUrl={photo.imgUrl}
+            />
+          ))}
+        </div> :
+        <p className='mt-4 text-lg antialiased text-balance text-center'>Nenhuma foto encontrada</p>
+      }
     </div>
   )
 }
