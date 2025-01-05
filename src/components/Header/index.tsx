e16f1bce1cdd4a3ad6df1121aefaffc543f5bee8
Josianne <anne.jmfreitas@gmail.com>
2025-01-04 20:47:45 -0400

    :bricks: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className='flex justify-center items-center bg-zinc-950 h-20'>
+      <h1 className='text-zinc-100 text-2xl font-semibold uppercase antialiased'>Galeria de Fotos</h1>
+    </div>
+  )
+}
